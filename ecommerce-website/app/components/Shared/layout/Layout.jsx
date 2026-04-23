24995a99e435bd523dabffbb22e99bbc20cfcaf2
Annas Ahmed <annasahmed1609@gmail.com>
2026-04-23 18:14:54 +0500

    update-add-optimisations-6

--- ecommerce-website/app/components/Shared/layout/Layout.jsx
+++ ecommerce-website/app/components/Shared/layout/Layout.jsx
@@ -1,13 +1,12 @@
 "use client";
-import { useStore } from "@/app/providers/StoreProvider";
+import AppProviders from "@/app/providers/AppProviders";
+import ReactQueryProvider from "@/app/providers/ReactQueryProvider";
+import { NextIntlClientProvider } from "next-intl";
 import Footer from "../../Themes/KidsTheme/Footer";
 import Navbar from "../../Themes/KidsTheme/Navbar";
 import BackToTop from "../BackToTop";
 import MobileBottomNav from "../MobileBottomNav";
 import WhatsAppButton from "../WhatsAppButton";
-import ReactQueryProvider from "@/app/providers/ReactQueryProvider";
-import { NextIntlClientProvider } from "next-intl";
-import AppProviders from "@/app/providers/AppProviders";
 import { AuthProvider } from "@/app/providers/AuthProvider";
 
 const Layout = ({ children, withFooter = true }) => {
