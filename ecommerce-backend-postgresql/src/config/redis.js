70f2bb11c629bc2c5b4054e9e2b2fda993183a0d
Annas Ahmed <annasahmed1609@gmail.com>
2026-04-23 14:52:59 +0500

    redis-working

--- ecommerce-backend-postgresql/src/config/redis.js
+++ ecommerce-backend-postgresql/src/config/redis.js
@@ -3,18 +3,19 @@ const redis = require('redis');
 const logger = require('./logger');
 
 // const redisClient = redis.createClient();
-
-const redisClient = '';
 // const redisClient = redis.createClient({
 // 	url: process.env.REDIS_URL || 'redis://localhost:6379', // default URL for Redis
 // });
+const redisClient = redis.createClient({
+	url: 'redis://127.0.0.1:6379',
+});
 
-// redisClient.connect().catch((err) => {
-// 	console.error('Redis connection error:', err);
-// });
+redisClient.connect().catch((err) => {
+	console.error('Redis connection error:', err);
+});
 
-// redisClient.on('connect', () => {
-// 	logger.info('Connected to Redis');
-// });
+redisClient.on('connect', () => {
+	logger.info('Connected to Redis');
+});
 
 module.exports = redisClient;
