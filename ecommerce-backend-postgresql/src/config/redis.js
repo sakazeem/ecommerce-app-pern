e9f56f51d1e314d5e377361fa592b148716081d2
Annas Ahmed <annasahmed1609@gmail.com>
2026-04-23 14:42:50 +0500

    add-redis-cache-disable

--- ecommerce-backend-postgresql/src/config/redis.js
+++ ecommerce-backend-postgresql/src/config/redis.js
@@ -3,16 +3,18 @@ const redis = require('redis');
 const logger = require('./logger');
 
 // const redisClient = redis.createClient();
-const redisClient = redis.createClient({
-	url: process.env.REDIS_URL || 'redis://localhost:6379', // default URL for Redis
-});
 
-redisClient.connect().catch((err) => {
-	console.error('Redis connection error:', err);
-});
+const redisClient = '';
+// const redisClient = redis.createClient({
+// 	url: process.env.REDIS_URL || 'redis://localhost:6379', // default URL for Redis
+// });
 
-redisClient.on('connect', () => {
-	logger.info('Connected to Redis');
-});
+// redisClient.connect().catch((err) => {
+// 	console.error('Redis connection error:', err);
+// });
+
+// redisClient.on('connect', () => {
+// 	logger.info('Connected to Redis');
+// });
 
 module.exports = redisClient;
