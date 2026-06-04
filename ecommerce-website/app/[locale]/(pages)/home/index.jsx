87abc8d26cc5e3b4aac1bc14319cec22e873ce53
Annas Ahmed <annasahmed1609@gmail.com>
2026-06-04 22:37:12 +0500

    fix-css

--- ecommerce-website/app/[locale]/(pages)/home/index.jsx
+++ ecommerce-website/app/[locale]/(pages)/home/index.jsx
@@ -12,6 +12,18 @@ export default async function HomePage() {
 				href={"https://cdn.babiesnbaba.com/web-3-1778582352761.webp"}
 				fetchPriority="high"
 			/>
+			<link
+				rel="preload"
+				as="image"
+				href={"https://cdn.babiesnbaba.com/summer-arrival-1779191900854.webp"}
+				fetchPriority="high"
+			/>
+			<link
+				rel="preload"
+				as="image"
+				href={"https://cdn.babiesnbaba.com/web-1-1778582375270.webp"}
+				fetchPriority="high"
+			/>
 			<HomepageContent homepageSections={homepageSections} />
 		</>
 	);
